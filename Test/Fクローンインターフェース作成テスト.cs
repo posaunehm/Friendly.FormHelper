84c998535f8f869c7a8f2c102e339078bb07d7f2
posaunehm <posaunehm@gmail.com>
2014-03-09 17:54:25 +0900

    Add t4 file which generates From-Clone interfaces(Value type only)

---
+++ Test/Fクローンインターフェース作成テスト.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class Fクローンインターフェース作成テスト
+    {
+        [TestMethod]
+        public void Formのクローンインターフェースを作成する()
+        {
+            var formType = typeof (Form);
+
+            string output = "";
+
+            foreach (var props in formType.GetProperties())
+            {
+                output += string.Format("{0} {1} {2}{3}",props.PropertyType.FullName,props.Name,"{get;set;};",Environment.NewLine);
+            }
+
+            
+
+        }
+    }
+
+    class testclass
+    {
+        public string Hoge { get; set; }
+    }
+
+
+}
